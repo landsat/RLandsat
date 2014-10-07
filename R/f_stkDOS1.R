254fce978e6ca271852b600d7674d525312e2e10
t3500 <bwtian@gmail.com>
2014-10-07 16:08:43 +0900

    + ‘.Rprofile’ + ‘.gitignore’ + ‘R/complete_code.R’ / ‘R/create_files.R’ + ‘R/create_roimask.R’ + ‘R/create_wekafiles.R’ + ‘R/f_applymask.R’ + ‘R/f_applytopocor.R’ + ‘R/f_getBandname.R’ + ‘R/f_jmdistance.R’ ...

---
+++ R/f_stkDOS1.R
@@ -0,0 +1,15 @@
+#' Apply ROI crop to DOS1 bands
+#' Song et al 2000: DOS1 Works just fine for land change detection
+#' 
+f.stkDOS1 <- function(roi = roi){
+  bands <- list.files(file.path(dir.work, dir.fun), full.names = T,
+                      pattern = '.TIF$')  
+  i.stk <- stack(bands)
+  i.stk <- crop(i.stk, roi)
+  names(i.stk) <- paste0('b', substrBand(bands), '_ae')
+  return(i.stk)
+}
+
+stk_dos1 <- f.stkDOS1(roi = roi)
+stk_dos1mar2014 <- f.stkDOS1(roi = roi)
+plot(stk_dos1)
